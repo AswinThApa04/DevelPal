25773307fc7ddcfbb1f828cfb36f800e29e88c5b
AswinThApa04 <aswinthapa337@gmail.com>
2025-11-13 01:26:41 +0530

    Initial commit - setup backend for full-stack DevPal structure

---
+++ backend/controllers/userController.js
@@ -0,0 +1,62 @@
+const User = require('../models/userModel');
+const generateToken = require('../utils/generateToken');
+
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields');
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    username,
+    email,
+    password, 
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401); 
+    throw new Error('Invalid email or password');
+  }
+};
+
+
+const getUserProfile = async (req, res) => {
+  res.status(200).json(req.user);
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getUserProfile,
+};
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const { errorHandler } = require('./middleware/errorHandler'); 
+
+const userRoutes = require('./routes/userRoutes');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/api/users', userRoutes);
+app.get('/api/ping', (req, res) => {
+  res.status(200).json({ message: 'pong' });
+});
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
